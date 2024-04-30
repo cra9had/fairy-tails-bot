373188a32a8dc96b474c791569c4788275b2645a
Khaiam <khaiam@mail.ru>
2024-04-30 12:44:28 +0300

    find dotenv added

--- bot/worker/settings.py
+++ bot/worker/settings.py
@@ -1,14 +1,14 @@
 import os
 
 from arq.connections import RedisSettings
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 from bot.worker.functions import shutdown, startup, send_tail_to_user_task, send_episode_to_user_task, \
     send_tail_plan_to_user_task
 
-load_dotenv(dotenv_path='.env')
-
 REDIS_HOST = os.getenv('REDIS_HOST')
 REDIS_PORT = int(os.getenv('REDIS_PORT'))
+
+load_dotenv(find_dotenv())
 
 
 class WorkerSettings:
