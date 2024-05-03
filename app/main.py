57e86d865c99c6dc4df9bf079ad359ea5e3e6baf
uiolo <vladyan198@gmail.com>
2024-05-03 17:44:29 +0300

    new docker file + requirements updated

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
+import os
+
 from fastapi import FastAPI, status
 from fastapi.requests import Request
+import aiohttp
+import logging
 
 
 app = FastAPI()
 
 @app.post("/")
 async def root(request: Request):
-    print(request.body())
+    async with aiohttp.ClientSession() as session:
+        logging.info(msg=await request.body())
+        async with session.get(
+                url=f'https://api.telegram.org/bot{os.getenv('BOT_TOKEN')}/sendMessage?chat_id=797228719&text=privet'
+        ) as responce:
+            pass
     return status.HTTP_200_OK
