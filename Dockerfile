4d455fee0ee4005308de831ce7b1de674c94bc96
uiolo <vladyan198@gmail.com>
2024-05-03 17:44:29 +0300

    new docker file + requirements updated

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ WORKDIR /code
 COPY ./requirements.txt /code/requirements.txt
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 COPY . .
-CMD alembic upgrade head && python3 -m bot
+CMD alembic upgrade head && python3 -m bot && arq bot.worker.settings.WorkerSettings && uvicorn app.main:app --proxy-headers --host 0.0.0.0 --port 8000
--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI, status
 from fastapi.requests import Request
+import aiohttp
+import logging
 
 
 app = FastAPI()
 
 @app.post("/")
 async def root(request: Request):
-    print(request.body())
+    async with aiohttp.ClientSession() as session:
+        logging.info(msg=await request.body())
+        async with session.get(
+                url='https://api.telegram.org/bot7175323516:AAG2dcrVeyhq5fsCgjeMmMIQv83Dh5XLpIU/sendMessage?chat_id=797228719&text=privet'
+        ) as responce:
+            pass
     return status.HTTP_200_OK
