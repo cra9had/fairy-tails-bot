1d135fb4555a0f6e9d8b2d8d8b70e6da35b6d1ef
uiolo <vladyan198@gmail.com>
2024-05-03 17:48:35 +0300

    Revert "new docker file + requirements updated"
    This reverts commit 4d455fee0ee4005308de831ce7b1de674c94bc96.

--- app/main.py
+++ app/main.py
@@ -1,17 +1,10 @@
 from fastapi import FastAPI, status
 from fastapi.requests import Request
-import aiohttp
-import logging
 
 
 app = FastAPI()
 
 @app.post("/")
 async def root(request: Request):
-    async with aiohttp.ClientSession() as session:
-        logging.info(msg=await request.body())
-        async with session.get(
-                url='https://api.telegram.org/bot7175323516:AAG2dcrVeyhq5fsCgjeMmMIQv83Dh5XLpIU/sendMessage?chat_id=797228719&text=privet'
-        ) as responce:
-            pass
+    print(request.body())
     return status.HTTP_200_OK
